ed2e1df806422340bf975fdcdf324592766bff62
dickyfahlevi21 <dicky.fahlevi21@gmail.com>
2020-08-27 04:43:33 +0700

    fixing styling

--- src/components/Albums/HomeAlbum.vue
+++ src/components/Albums/HomeAlbum.vue
@@ -4,7 +4,7 @@
       <paginate name="albumPagine" :list="dataAlbum" :per="6" ref="albumPagine" class="grid grid-cols-3">
         <div v-for="(listAlbum, i) in paginated('albumPagine')" :key="i" class="px-3 text-center">
           <img src="../../assets/img/dummy.png" class="w-8/12">
-          <router-link :to="`albums/${listAlbum.id}`" class="text-xl font-bold">
+          <router-link :to="`albums/${listAlbum.id}`" class="text-base font-bold">
             <h2 class="p-2 w-8/12">{{ listAlbum.title }}</h2>
           </router-link>
         </div>
--- src/components/SideBar/SideBar.vue
+++ src/components/SideBar/SideBar.vue
@@ -4,19 +4,19 @@
         <hr class="w-11/12 mb-4">
 
         <router-link to="/posts" class="side-color text-lg font-bold mt-1 p-2">Post</router-link>
-        <p class="sub-color text-base px-2 mb-4">
+        <p class="sub-color text-base pl-2 pr-4 mb-4">
             Lorem Ipsum is simply dummy text of the printing and typesetting industry.
             Lorem Ipsum has been the industry's standard dummy text ever since the 1500s
         </p>
 
         <router-link to="/photos" class="side-color text-lg font-bold mt-1 p-2">Photo</router-link>
-        <p class="sub-color text-base px-2 mb-4">
+        <p class="sub-color text-base pl-2 pr-4 mb-4">
             Lorem Ipsum is simply dummy text of the printing and typesetting industry.
             Lorem Ipsum has been the industry's standard dummy text ever since the 1500s
         </p>
 
         <router-link to="/albums" class="side-color text-lg font-bold mt-1 p-2">Album</router-link>
-        <p class="sub-color text-base px-2 mb-4">
+        <p class="sub-color text-base pl-2 pr-4 mb-4">
             Lorem Ipsum is simply dummy text of the printing and typesetting industry.
             Lorem Ipsum has been the industry's standard dummy text ever since the 1500s
         </p>
